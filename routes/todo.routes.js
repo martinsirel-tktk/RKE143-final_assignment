528a83ba44efd7fd04db28fe394b9bc981ec43ac
Martin <example@myemail.com>
2026-01-18 18:04:08 +0200

    add files

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,20 +1,21 @@
 const express = require('express');
-
+const db = require('../db');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
 
     try {
-        const data = db.query('SELECT * FROM todo;');
+        const data = await db.query('SELECT * FROM todo;');
         res.status(200).json({todo: data.rows});
     }
     catch(error) {
         console.log(error);
+        res.status(500).json({error: "Server error."});
     }
     
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     console.log(req.body);
     const { task } = req.body;
 
@@ -25,6 +26,7 @@ router.post('/', (req, res) => {
     } 
     catch (error) {
         console.log(error);
+        res.status(500).json({error: "Insert failed."});
     } 
 
 });
@@ -34,7 +36,7 @@ router.delete('/', async (req, res) => {
     const data = await db.query("SELECT * FROM todo WHERE id = $1;", [id]);
 
     if(data.rows.length === 0) {
-        res.json({message: "there no such task"});
+        res.json({message: "there is no such task"});
     } else {
         try {
             const result = await db.query("DELETE FROM todo WHERE id = $1;", [id]);
@@ -42,9 +44,10 @@ router.delete('/', async (req, res) => {
         }
         catch(error) {
             console.log(error);
+            res.status(500).json({error: "Delete failed."});
         }
     }
 });
 
 
-module.exports = ; 
+module.exports = router; 
